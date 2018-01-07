50923375ed86c01269a547a4200be3dd6995be45
CherishClark <cherish.g.clark@gmail.com>
2018-01-07 14:01:53 -0700

    rooted to index page, added index to controler, created menu_items for seeds file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :menu_items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: "menu_items#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do MenuItem.create!(main_image: 'http://via.placeholder.com/350x250',
+                             name: "Prince Burger",
+                             price: 7,
+                             category: "burgers",
+                             description: "quite juicy and meat-free"
+
+      )
+end
+
+puts "Created 10 menu items"
